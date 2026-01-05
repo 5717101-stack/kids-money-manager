04dc794a0113df11918827894f4b8e348514ee00
Itzik Bachar <itzikbachar@Itziks-iMac.local>
2026-01-05 15:48:21 +0200

    Update version to 1.13

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
       </main>
       
       <footer className="app-footer">
-        <span className="version">גרסה 1.12</span>
+        <span className="version">גרסה 1.13</span>
       </footer>
     </div>
   );
