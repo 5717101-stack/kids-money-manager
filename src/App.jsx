f823c1cc1d755bc4905419d7afb0ed2ce11ed4c8
Itzik Bachar <itzikbachar@Itziks-iMac.local>
2026-01-05 14:03:26 +0200

    Update version to 1.09

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
       </main>
       
       <footer className="app-footer">
-        <span className="version">גרסה 1.08</span>
+        <span className="version">גרסה 1.09</span>
       </footer>
     </div>
   );
