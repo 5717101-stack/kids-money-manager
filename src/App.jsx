9f862cbed4900749b5c974bf398362aae6cade5c
Itzik Bachar <itzikbachar@Itziks-iMac.local>
2026-01-05 17:00:10 +0200

    Update version to 1.16

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
       </main>
       
       <footer className="app-footer">
-        <span className="version">גרסה 1.15</span>
+        <span className="version">גרסה 1.16</span>
       </footer>
     </div>
   );
