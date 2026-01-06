cc9264de0f066265efee124b95689f8ce8ac5ff6
Itzik Bachar <itzikbachar@Itziks-iMac.local>
2026-01-06 14:22:48 +0200

    Update version to 2.2.0

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
       </main>
       
       <footer className="app-footer">
-        <span className="version">גרסה 2.1</span>
+        <span className="version">גרסה 2.2</span>
       </footer>
     </div>
   );
