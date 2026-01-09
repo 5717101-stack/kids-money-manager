ed51dd9581fafb0a9ea618f50d9a7aee496df505
Bachar Israeli <bachar.israeli@example.com>
2026-01-09 12:46:14 +0200

    Remove rounded rectangle from allowance-info section (v3.4.36)

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -256,7 +256,7 @@ const WelcomeScreen = ({ onSelectCreate, onSelectJoinAsParent, onSelectJoinAsChi
             🗑️ מחק הכל
           </button>
         </div>
-        <span className="version">{t('common.version')} 3.4.35</span>
+        <span className="version">{t('common.version')} 3.4.36</span>
       </footer>
       
       {showUsersTable && (
